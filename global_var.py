aed218c8de9219d1b9d95b87ff472a5bf4116bde
John Chen <tetlun54@gmail.com>
2021-04-16 11:35:23 +0800

    first commit

---
+++ global_var.py
@@ -0,0 +1,11 @@
+# Separate file to hold the shared variables
+
+nat_type = "N/A"
+external_ip = "N/A"
+external_port = "N/A"
+busy = False
+
+speed_test_src = "N/A"
+speed_test_host = "N/A"
+speed_test_dl = "N/A"
+speed_test_up = "N/A"
---
+++ main.py
@@ -0,0 +1,46 @@
+import threading, speedtest, global_var as g
+
+from kivymd.app import MDApp
+from kivy.lang import Builder
+from kivymd.uix.label import MDLabel
+from kivymd.uix.button import MDRectangleFlatButton
+from kivy.app import App
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.button import Button
+from kivy.uix.label import Label
+from kivy.clock import Clock
+from kivymd.uix.menu import MDDropdownMenu
+from kivymd.uix.snackbar import Snackbar
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.core.window import Window
+
+# SSL certificate for HTTPS request
+import certifi
+import os
+os.environ['SSL_CERT_FILE'] = certifi.where()
+
+class SpeedTest_Screen(Screen):
+    def getSpeedTest(self):
+        self.loading_button.active = True
+        self.req_button.opacity = 0
+        self.req_button.disabled = 1
+        t1 = threading.Thread(target=self.retSpeedTest).start()
+        print(f"ret : {g.speed_test_src}, {g.speed_test_host}, {g.speed_test_dl}, {g.speed_test_up}")
+
+    def retSpeedTest(self):
+        g.speed_test_src, g.speed_test_host, g.speed_test_dl, g.speed_test_up = speedtest.main()
+        self.ret_speedtest.text = g.speed_test_src+"\n\n"+g.speed_test_host+"\n\n"+g.speed_test_dl+"\n\n"+g.speed_test_up
+        self.loading_button.active = False
+        self.next_button.opacity = 1
+        self.next_button.disabled = 0
+    pass
+
+class MainApp(MDApp):
+    def build(self):
+        # Theming : https://kivymd.readthedocs.io/en/latest/themes/theming/index.html
+        self.theme_cls.primary_palette = "Blue"
+        # self.theme_cls.theme_style = "Light"
+
+if __name__ == "__main__":
+    MainApp().run()
